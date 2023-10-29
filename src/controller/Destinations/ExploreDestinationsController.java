32f0487ec9ad88ae13f99624787ab4ea5b6eb965
Maxwell Tran <maxwell.tran@student.uts.edu.au>
2023-10-30 01:39:14 +1100

    30/10 1:39 AM

--- src/controller/Destinations/ExploreDestinationsController.java
+++ src/controller/Destinations/ExploreDestinationsController.java
@@ -1,7 +1,64 @@
 package controller.Destinations;
 
 
-public class ExploreDestinationsController {
+import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import model.Destinations;
+import model.Exceptions.ErrorModel;
 
+import java.io.IOException;
+
+
+public class ExploreDestinationsController extends Controller<Destinations> {
+    @FXML
+    Button ViewAllDestinations;
+    @FXML
+    Button ViewFilterDestinations;
+    @FXML
+    Button AddDestination;
+    @FXML
+    Button RemoveDestination;
+    @FXML
+    Button DestinationExit;
+
+    @FXML
+    public void handleDestinationAllView(){
+        try {
+            ViewLoader.showStage(model, "/view/Destinations/DisplayDestinationsView.fxml", "All Destinations", new Stage());
+        } catch (IOException e) {
+            ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error"));
+        }
+    }
+    @FXML
+    public void handleDestinationFilterView(){
+        try {
+            ViewLoader.showStage(model, "/view/Destinations/DisplayFilteredDestinationsView.fxml", "Filtered Destinations", new Stage());
+        } catch (IOException e) {
+            ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error"));
+        }
+    }
+    @FXML
+    public void handleDestinationAdd(){
+        try {
+            ViewLoader.showStage(model, "/view/Destinations/AddDestinationView.fxml", "Add Destination", new Stage());
+        } catch (IOException e) {
+            ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error"));
+        }
+    }
+    @FXML
+    public void handleDestinationRemove(){
+        try {
+            ViewLoader.showStage(model, "/view/Destinations/RemoveDestinationView.fxml", "Remove Destination", new Stage());
+        } catch (IOException e) {
+            ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error"));
+        }
+    }
+    @FXML
+    public void handleDestinationExit(){
+        stage.close();
+    }
    
 }
