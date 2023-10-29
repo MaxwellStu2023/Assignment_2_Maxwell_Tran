f81b08015a79575130646adac6b109b93bb2eed0
Maxwell Tran <maxwell.tran@student.uts.edu.au>
2023-10-29 16:13:50 +1100

    29/10 4:13 PM

--- src/controller/Flights/DisplayFlightsController.java
+++ src/controller/Flights/DisplayFlightsController.java
@@ -1,7 +1,18 @@
 package controller.Flights;
 
 
+import au.edu.uts.ap.javafx.Controller;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import model.Flight;
+import model.Flights;
 
-public class DisplayFlightsController  {
-    
+import java.util.Observable;
+
+public class DisplayFlightsController extends Controller<Flights> {
+
+    private Flights getFlights(){
+        return model;
+    }
 }
--- src/controller/Flights/ExploreFlightsController.java
+++ src/controller/Flights/ExploreFlightsController.java
@@ -3,13 +3,13 @@ package controller.Flights;
 
 import au.edu.uts.ap.javafx.Controller;
 import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import model.Exceptions.ErrorModel;
 import model.Flights;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class ExploreFlightsController extends Controller<Flights> {
@@ -25,48 +25,47 @@ public class ExploreFlightsController extends Controller<Flights> {
     Button FlightExit;
 
     @FXML
-    private void handleFlightAllView(ActionEvent event){
+    private void handleFlightAllView(ActionEvent event) {
         try {
             ViewLoader.showStage(model, "/view/Flights/DisplayFlightsView.fxml", "View All Flights", new Stage());
         } catch (IOException e) {
-            ViewLoader.showErrorWindow(new ErrorModel(e,"IO Exception Error"));
+            ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error"));
         }
     }
 
     @FXML
-    private void handleFlightFilterView(ActionEvent event){
+    private void handleFlightFilterView(ActionEvent event) {
         try {
             ViewLoader.showStage(model, "/view/Flights/DisplayFilteredFlightsView.fxml", "View All Flights", new Stage());
         } catch (IOException e) {
-            ViewLoader.showErrorWindow(new ErrorModel(e,"IO Exception Error"));
+            ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error"));
         }
     }
 
     @FXML
-    private void handleFlightAdd(ActionEvent event){
+    private void handleFlightAdd(ActionEvent event) {
         try {
             ViewLoader.showStage(model, "/view/Flights/AddFlightView.fxml", "View All Flights", new Stage());
         } catch (IOException e) {
-            ViewLoader.showErrorWindow(new ErrorModel(e,"IO Exception Error"));
+            ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error"));
         }
     }
 
     @FXML
-    private void handleFlightRemove(ActionEvent event){
+    private void handleFlightRemove(ActionEvent event) {
         try {
-        ViewLoader.showStage(model, "/view/Flights/RemoveFlightView.fxml", "View All Flights", new Stage());
+            ViewLoader.showStage(model, "/view/Flights/RemoveFlightView.fxml", "View All Flights", new Stage());
         } catch (IOException e) {
-        ViewLoader.showErrorWindow(new ErrorModel(e,"IO Exception Error"));
+            ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error"));
         }
     }
 
     @FXML
-    private void handleFlightExit(ActionEvent event){
-        try {
-        ViewLoader.showStage(model, "/view/Flights/DisplayFlightsView.fxml", "View All Flights", new Stage());
-        } catch (IOException e) {
-        ViewLoader.showErrorWindow(new ErrorModel(e,"IO Exception Error"));
+    private void handleFlightExit(ActionEvent event) {
+        stage.close();
         }
-    }
 }
+
+
+
 
