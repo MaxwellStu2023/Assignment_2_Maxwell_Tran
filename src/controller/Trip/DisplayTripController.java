d3820dcf3054feb0a3b8f42accbc494b0cd5a4f3
MaxwellStu2023 <maxwell.tran@student.uts.edu.au>
2023-10-31 23:46:42 +1100

    31/10 11:46 PM

--- src/controller/Trip/DisplayTripController.java
+++ src/controller/Trip/DisplayTripController.java
@@ -26,11 +26,12 @@ public class DisplayTripController extends Controller<Trip> {
     Button CloseTripView;
     @FXML
     Button ViewIndividual;
-    private Trip getTrip(){
+
+    private Trip getTrip() {
         return model;
     }
 
-    public void initialize(){
+    public void initialize() {
         TripList.setItems(model.getItinery());
         TripList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         TripList.getSelectionModel().selectedItemProperty().addListener(observable -> getCurrent());
@@ -38,31 +39,37 @@ public class DisplayTripController extends Controller<Trip> {
     }
 
     @FXML
-    private void handleCloseTripView(ActionEvent event){
+    private void handleCloseTripView(ActionEvent event) {
         stage.close();
     }
 
-    public ObservableList<Itinery> getCurrent(){
+    public ObservableList<Itinery> getCurrent() {
         return TripList.getSelectionModel().getSelectedItems();
 
     }
+
     @FXML
-    private void handleViewIndividual(ActionEvent event){
-            if (getCurrent().contains(model.getDestinations())) {
-                try {
-                    System.out.println("Hello World");
-                    ViewLoader.showStage(model.getDestinations(), "/view/Destinations/DisplayDestinationView.fxml", "View Individual Destinations", new Stage());
-                } catch (IOException e) {
-                    ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error!"));
+    private void handleViewIndividual(ActionEvent event) {
+        if (getCurrent() != null) {
+            try {
+                if (getCurrent().equals(model.getDestinations())) {
+                    try {
+                        System.out.println("Hello World");
+                        ViewLoader.showStage(model.getDestinations(), "/view/Destinations/DisplayDestinationView.fxml", "View Individual Destinations", new Stage());
+                    } catch (IOException e) {
+                        ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error!"));
+                    }
+                } else if (getCurrent().equals(model.getFlights())) {
+                    try {
+                        System.out.println("Hello World");
+                        ViewLoader.showStage(model.getFlights(), "/view/Flights/DisplayFlightsView.fxml", "View Individual Flights", new Stage());
+                    } catch (IOException e) {
+                        ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error!"));
+                    }
                 }
-            }
-            else if (getCurrent().contains(model.getFlights())) {
-                try {
-                    System.out.println("Hello World");
-                    ViewLoader.showStage(model.getFlights(), "/view/Flights/DisplayFlightsView.fxml", "View Individual Flights", new Stage());
-                } catch (IOException e) {
-                    ViewLoader.showErrorWindow(new ErrorModel(e, "IO Exception Error!"));
-                }
+            } catch (InputMismatchException e) {
+                ViewLoader.showErrorWindow(new ErrorModel(e, "You can't display Flights and Destination together!"));
             }
         }
+    }
 }
